dccea040a990b372bacdfebf0dc2d8a6f273bebf
Aleksander <pironaleksander@gmail.com>
2019-09-17 13:34:27 +0200

    sss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  state={
+    allItems:[
+      'speakers',
+      'plastic fork',
+      'television',
+      'doll',
+      'box',
+      'wagon',
+      'flag',
+      'shoe lace',
+      'phone',
+      'buckel',
+      'fake flowers',
+      'USB drive',
+      'eraser',
+      'toothbrush',
+      'keyboard',
+      'rubber duck',
+    ],
+    filteredItems:[
+      'speakers',
+      'plastic fork',
+      'television',
+      'doll',
+      'box',
+      'wagon',
+      'flag',
+      'shoe lace',
+      'phone',
+      'buckel',
+      'fake flowers',
+      'USB drive',
+      'eraser',
+      'toothbrush',
+      'keyboard',
+      'rubber duck',
+    ],
+    phrase:''
+  }
+  filterChangeHandler=()=>{
+    const val = this.state.phrase;
+    const updated = this.state.allItems.filter(item=>{
+      if(item.includes(val)){
+          return item;
+      }
+    })
+    this.setState({
+      filteredItems:updated
+    })
+  }
+  inputHandler=(e)=>{
+    this.setState({
+      phrase:e.target.value
+    }, this.filterChangeHandler)
+  }
+  render(){
+    return (
+      <div className="App">
+        <input type="text" onChange={this.inputHandler}/>
+        <ul>
+            {this.state.filteredItems.map((item, index)=>{
+              const val = this.state.phrase;
+              item = item.replace(val, '<span class="red">'+val+'</span>');
+              return <li key={'a'+index} dangerouslySetInnerHTML={{__html:item}} />
+            })}
+        </ul>
     </div>
   );
+}
 }
 
 export default App;
